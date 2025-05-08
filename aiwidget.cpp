9354356620d9d4cf16feb01154d3cb6378d68cca
youMCYT <MC_LupinIII@outlook.com>
2025-05-08 14:49:11 +0800

    实现聊天界面的基础功能

--- aiwidget.cpp
+++ aiwidget.cpp
@@ -6,9 +6,31 @@ AIWidget::AIWidget(QWidget *parent)
     , ui(new Ui::AIWidget)
 {
     ui->setupUi(this);
+
+    connect(ui->sendButton, &QPushButton::clicked, this, &AIWidget::sendMessage);
 }
 
 AIWidget::~AIWidget()
 {
     delete ui;
+}
+
+void AIWidget::addMessage(const QString &message, bool is_self)
+{
+    QLabel *msgLabel = new QLabel(message);
+    msgLabel->setWordWrap(true);
+    msgLabel->setAlignment(is_self ? Qt::AlignRight : Qt::AlignLeft);
+
+    ui->messageLayout->addWidget(msgLabel);
+}
+
+void AIWidget::sendMessage()
+{
+    QString msg = ui->inputEdit->toPlainText();
+
+    if (!msg.isEmpty())
+    {
+        addMessage(msg);
+        ui->inputEdit->clear();
+    }
 }
--- aiwidget.h
+++ aiwidget.h
@@ -2,6 +2,10 @@
 #define AIWIDGET_H
 
 #include <QWidget>
+#include <QString>
+#include <QLabel>
+#include <QTimer>
+#include <QPushButton>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -17,7 +21,12 @@ public:
     AIWidget(QWidget *parent = nullptr);
     ~AIWidget();
 
+private slots:
+    void sendMessage();
+    void addMessage(const QString &message, bool is_self = true);
+
 private:
     Ui::AIWidget *ui;
 };
+
 #endif // AIWIDGET_H
